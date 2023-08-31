392c2a299e8017f02cbfabc64751548afb8c095d
Copper2 <107405279+long-1810@users.noreply.github.com>
2023-08-31 21:01:48 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/REST-countries-API-with-color-switcher/',
   plugins: [react()],
 });
