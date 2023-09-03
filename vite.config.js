2267a434812b04ea948d47ee05c6d4efde060aac
Copper2 <107405279+long-1810@users.noreply.github.com>
2023-09-03 17:01:33 +0700

    Prepare to deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/REST-countries-API-with-color-switcher/"
 });
